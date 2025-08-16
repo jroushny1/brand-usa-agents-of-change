954d13d9e7fa014086006a2078566b33c4e993d9
jroushny1 <128194875+jroushny1@users.noreply.github.com>
2025-08-16 16:23:20 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['image.mux.com'],
+  },
+  env: {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
+  },
+}
 
+module.exports = nextConfig
