5a4436828bda5442c88e5858b2ddc72e77f30638
jroushny1 <128194875+jroushny1@users.noreply.github.com>
2025-09-10 18:12:54 -0400

    Update page.tsx

--- src/app/enter/page.tsx
+++ src/app/enter/page.tsx
@@ -15,9 +15,9 @@ export default function EnterPage() {
     setLoading(true)
     
     // Check if code is correct (case insensitive)
-    if (code.toLowerCase() === 'AmericaTheBeautiful') {
+    if (code.toLowerCase() === 'americathebeautiful') {
       // Set cookie and redirect
-      document.cookie = `partner_access=AmericaTheBeautiful; path=/; max-age=${60 * 60 * 24 * 30}` // 30 days
+      document.cookie = `partner_access=americathebeautiful; path=/; max-age=${60 * 60 * 24 * 30}` // 30 days
       window.location.href = '/'
     } else {
       setError(true)
